58088d633d1c9e4878ded5f0137fd7d3caf3d5d0
Tanachi <tanachige@gmail.com>
2016-09-30 19:09:43 -1000

    Started making the databases with mongo

---
+++ dbSetup.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const MONGO_URL = 'mongodb://localhost/omnimood';
+const connection = mongoose.connect(MONGO_URL);
+const data = require('./countryList.json');
+const country = require('./countries');
+
+mongoose.connection.once('open', function() {
+  data.forEach(function(element, index, array) {
+    const newCountry = new country({
+      countryId: index,
+      name: element.name,
+      code: element.code,
+      GPS: '0,0',
+      Mood: 'Happy',
+      Emoji: 'Nothing'
+    });
+    newCountry.save();
+  });
+  console.log('finished inserting');
+});
