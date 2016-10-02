2491408debddf3fb8973c43a35f39df6153531d0
Spacekins <spacekins@zoho.com>
2016-10-01 20:27:06 -1000

    added outline and highlight of countries

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,12 @@
    width = +svg.attr("width"),
    height = +svg.attr("height");
 
+  var outlineDefault="#eeeeee";
+  var outlineHighlight="#1221ee";
+  var fillDefault="#00bb00";
+
+  var testText=d3.select("body").append("div").attr("id","testText");
+
 
 
  d3.json("json/world-50m.json", function(error, world) {
@@ -22,5 +28,42 @@
        return "cc" + d.id;
      })
      .attr("d", path)
-     .style("fill", "#000000");
+     .attr("stroke",outlineDefault)
+     .style("fill", fillDefault)
+     .on("mouseover",function() {
+       d3.select(this)
+       .attr("stroke",outlineHighlight)
+     })
+     .on("mouseout",function(){
+       d3.select(this)
+       .attr("stroke",outlineDefault)
+     })
+     .append("svg:title")
+   .text(function(d) { return "TEST"; });
+
+    setCountryMood(840,"#0ff0d0");
+    setCountryMood(258, "#fff0d0");
+    setCountryMood(124,"#f0d0ff");
  });
+
+
+ function setCountryMood(id,mood){
+  console.log("TEst In Mood");
+  svg.select("path#cc"+id)
+  .data([1,1,2])
+  .style("fill", mood);
+ }
+
+ /*
+ 
+ .on("mouseover", function() {
+  d3.select(this)
+    .attr('fill', '') // Un-sets the "explicit" fill (might need to be null instead of '')
+    .classed("active", true ) // should then accept fill from CSS
+})
+.on("mouseout",  function() {
+  d3.select(this)
+    .classed("active", false)
+    .attr('fill', function(d) { return z(d.count_shipments); }) // Re-sets the "explicit" fill
+  });
+  */
