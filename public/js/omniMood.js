c2d1bea1311122acb9fdd8ae448e8c3d741764de
javenkn <javenkn7@yahoo.com>
2016-10-01 17:33:00 -1000

    deleted the controller that was there before

--- public/js/omniMood.js
+++ public/js/omniMood.js
@@ -1,8 +1,3 @@
 angular.module('omniMood', []);
 
 var omniMood = angular.module('omniMood');
-
-omniMood.controller('titleController', ['$scope', function ($scope) {
-  $scope.title = 'Omni Mood';
-}
-]);
