50dd4ac1b876e9f95ae11b5ce1fe3140fb4e0519
kekuso <kekuso808@gmail.com>
2016-10-04 18:55:49 -1000

    Added set timeout when stream errors in twitter.js

---
+++ faker.js
@@ -0,0 +1,32 @@
+var faker = require('faker');
+
+var fakeData = [];
+
+var latitude = faker.address.latitude();
+var longitude = faker.address.longitude();
+var city = faker.address.city();
+var country = faker.address.country();
+var countryCode = faker.address.countryCode();
+
+function generateFakeData () {
+  fakeData.push(
+    {
+      text: [],
+      coordinates:
+        {
+          lat: latitude,
+          long: longitude
+        },
+      date: new Date().toLocaleString(),
+      type: "Point",
+      place: city,
+      country_code: countryCode,
+      country: country
+    }
+  );
+  console.log(fakeData);
+  setTimeout(generateFakeData, 1000);
+}
+
+module.exports = generateFakeData();
+
