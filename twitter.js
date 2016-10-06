fb34bcafaac1c34652682b7af2822f5cda56b699
Tanachi <tanachige@gmail.com>
2016-10-05 18:00:53 -1000

    Added comments

--- twitter.js
+++ twitter.js
@@ -20,7 +20,7 @@ twitter.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function (str
         var text = tweet.text;
         text = text.match(new RegExp(ranges.join('|'), 'g'));
         if(text) { // if there's an emoji found
-          if(tweet.place.country){
+          if(tweet.place.country){ //if there is country location data(still crashes)
             tweets.push(
               {
                 text: text,
@@ -32,7 +32,7 @@ twitter.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function (str
                 country: tweet.place.country
               }
             );
-            var mood = 0;
+            var mood = 0; // total mood for the tweet
             // surrogate pairs: (output like this)
             // multiple emojis: [ '\\uD83D\\uDE04', '\\uD83D\\uDC96', '\\uD83D\\uDE3B' ]
             // only one emoji: [ '\\uD83D\\uDE02' ]
@@ -42,13 +42,13 @@ twitter.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function (str
               return '\\u' + emoji.charCodeAt(0).toString(16).toUpperCase() + '\\u' + emoji.charCodeAt(1).toString(16).toUpperCase();
             });
             // printing surrogate pairs
-            surrogate.forEach((surrogate) => {
-              if(emojiList[surrogate]){
-                if(codeTweets[emojiList[surrogate].name]){
+            surrogate.forEach((surrogate) => { // goes through each emote to check our emojilist
+              if(emojiList[surrogate]){ // if there is a match with our emojilist
+                if(codeTweets[emojiList[surrogate].name]){ // If there already is a key with the name of the emoji
                   codeTweets[emojiList[surrogate].name] =  codeTweets[emojiList[surrogate].name] + 1;
                 }
-                else{
-                  mood += emojiList[surrogate].value;
+                else{ // IF there is no key with the emoji name
+                  mood += 1;
                   codeTweets[emojiList[surrogate].name] = 1;
                 }
               }
@@ -56,24 +56,25 @@ twitter.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function (str
               var code = '0x';
               // console.log(String.fromCharCode(code+surrogatePair[0], code+surrogatePair[1]));
             });
-            if(Object.keys(codeTweets).length!== 0){
-              if(tweetUpdate[tweet.place.country]){
-                for(var pairs in codeTweets){
+            if(Object.keys(codeTweets).length!== 0){ // If there is at least 1 emoji from our list
+              if(tweetUpdate[tweet.place.country]){ // If there is already a country key in the object
+                for(var pairs in codeTweets){ // goes though the recent tweet and it to the total countries.
                   var updateCountry = tweetUpdate[tweet.place.country];
-                  if(updateCountry[pairs])
+                  if(updateCountry[pairs]) // Update each value to add to the emoji count
                     updateCountry[pairs] += codeTweets[pairs];
-                  else{
+                  else{ // IF theres a new emoji, set it to 1
                     updateCountry[pairs] = 1;
                   }
                 }
-                tweetUpdate[tweet.place.country].mood += mood;
+                tweetUpdate[tweet.place.country].mood += mood; // combines the mood scores of both
               }
-              else{
+              else{ // If there is no country key in the object
+                // sets the country twitter and mood to the new tweet
                 tweetUpdate[tweet.place.country] = codeTweets;
                 tweetUpdate[tweet.place.country].mood = mood;
               }
-              tweetCount --;
-              if(tweetCount === 0){
+              tweetCount --; // Decreases the tweeter count before sending to server.
+              if(tweetCount === 0){ //sends the tweet data and resets the count
                 tweetCount = 15;
                 console.log(tweetUpdate);
                 tweetUpdate = {};
