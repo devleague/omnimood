4b47a5fff1b7497c02744a3589dcafe03fa81bc5
Spacekins <spacekins@zoho.com>
2016-10-06 21:53:32 -1000

    add group layers to svg

--- public/js/map.js
+++ public/js/map.js
@@ -1,13 +1,13 @@
- var svg = d3.select("svg"),
-   width = +svg.attr("width"),
-   height = +svg.attr("height");
+ var svg = d3.select("svg#svg_map"),
+  width = (document.body.clientWidth*.85),
+  height = (document.body.clientHeight);
 
  var outlineDefault = "#eeeeee";
  var outlineHighlight = "#1221ee";
  var fillDefault = "#000000";
 
- var moodMin = 0;
- var moodMid = 5;
+ var moodMin = -10;
+ var moodMid = 0;
  var moodMax = 10;
 
  var testText = d3.select("body").append("div").attr("id", "testText");
@@ -21,15 +21,15 @@
 
    d3.select("svg#svg_map")
       .append("rect")
-      .attr("width","100%")
-      .attr("height","100%")
+      .attr("width",width)
+      .attr("height",height)
       .style("fill","steelblue");
       //.attr("background-color","steelblue");
 
  d3.json("json/world-50m.json", function(error, world) {
 
    var countries = topojson.feature(world, world.objects.countries).features
-   
+
    var projection = d3.geoMercator()
      .scale((width - 3) / (2 * Math.PI))
      .translate([width / 2, height / 2]);
@@ -53,7 +53,7 @@
           {
             return fillDefault;
           }
-       
+
      }) //fillDefault
      .on("mouseover", function(d) {
        d3.select(this)
@@ -89,15 +89,11 @@
      ;
  }
 
- 
- 
   setInterval(function() {
     var thisCountryObject = testCountryJSON[Math.floor((Math.random() * testCountryJSON.length))];
-   // console.log(thisCountryObject);
-   // displayCountry(thisCountryObject.c);
-    setCountryMood(thisCountryObject.id, moodScale(Math.floor((Math.random() * moodMax))));
-  }, 100);
- 
+    setCountryMood(thisCountryObject.id, moodScale(Math.floor((Math.random() * (moodMax - moodMin)) - 10)));
+  }, 500);
+
 
  var testCountryJSON = [{
    "id": "4",
