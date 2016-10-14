642fccceb287659ee46b05aef27f62731b0e26db
javenkn <javenkn7@yahoo.com>
2016-10-13 19:59:50 -1000

    adds toggle view controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -10,14 +10,22 @@ angular.module('omniMood')
       });
     }
   ])
-  .controller('emojiController', ['$scope', 'EmojiFactory', function($scope, EmojiFactory) {
-    // console.log(EmojiFactory);
-    $scope.Emojis = [];
-    EmojiFactory.getEmojis()
-      .then(function(emojis) {
-        // console.log(data);
-        emojis.data.forEach(function (code) {
-          $scope.Emojis.push(code);
-        })
-      })
-  }]);
+  .controller('emojiController', [
+    '$scope',
+    'EmojiFactory',
+    function($scope, EmojiFactory) {
+      $scope.Emojis = [];
+      EmojiFactory.getEmojis()
+        .then(function(emojis) {
+          emojis.data.forEach(function (code) {
+            $scope.Emojis.push(code);
+          });
+        });
+    }
+  ])
+  .controller('toggleViewController', function ($scope) {
+    $scope.show = true;
+    $scope.$watch('show', function () {
+      $scope.toggleText = $scope.show ? '3D View' : '2D View';
+    });
+  });
