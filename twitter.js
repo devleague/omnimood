5ee1e774c99a631d8541c466a5db9ea04fa0eb6f
javenkn <javenkn7@yahoo.com>
2016-10-15 00:05:38 -1000

    fixed little bug with the code

--- twitter.js
+++ twitter.js
@@ -45,13 +45,14 @@ function getVariables(tweet) {
 function listenForTweets(socket) {
   socket.on('start tweets', () => {
     twitterStream.on(('data'), function (tweet) {
-      var emojis = getVariables(tweet).emojis;
-      if(emojis) { // if there's an emoji found
-        socket.emit('tweet', {
-          emojis: emojis,
-          coordinates: coordinates
-        });
-      }
+      var tweetObj = getVariables(tweet);
+      if(tweetObj)
+        if(tweetObj.emojis) { // if there's an emoji found
+          socket.emit('tweet', {
+            emojis: tweetObj.emojis,
+            coordinates: tweetObj.coordinates
+          });
+        }
     });
 
     twitterStream.on('error', function (error) {
