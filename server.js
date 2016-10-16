ad9f327c4c2774598296bf90ee69984dfc80ff58
Tanachi <tanachige@gmail.com>
2016-10-15 15:20:11 -1000

    Added mood percentage

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get('/graphs', (req, res)=>{
   res.sendFile(path.join(__dirname+'/public/graphs.html'));
 });
 
-app.get('/countries/api', (req, res) => {
+app.get('/api/countries', (req, res) => {
   Country
   .find({})
   .then(results => res.json(results));
