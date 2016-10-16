2dd1c438286b781d0d941418d1390d5887b40665
kekuso <kekuso808@gmail.com>
2016-10-15 15:12:31 -1000

    Created separate emojiList.html file. Added grayscale and color animations.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,19 +2,15 @@ angular.module('omniMood')
   .controller('tweetController', [
     '$scope',
     'socket',
-    function ($scope, socket) {
+    'EmojiFactory',
+    function ($scope, socket, EmojiFactory) {
       $scope.Tweets = [];
       socket.emit('start tweets', true);
       socket.on('tweet', function (tweet) {
         $scope.coordinates = tweet.coordinates;
         $scope.Tweets.push(tweet.emojis);
       });
-    }
-  ])
-  .controller('emojiController', [
-    '$scope',
-    'EmojiFactory',
-    function($scope, EmojiFactory) {
+
       $scope.Emojis = [];
       EmojiFactory.getEmojis()
         .then(function(emojis) {
@@ -24,6 +20,19 @@ angular.module('omniMood')
         });
     }
   ])
+  // .controller('emojiController', [
+  //   '$scope',
+  //   'EmojiFactory',
+  //   function($scope, EmojiFactory) {
+  //     $scope.Emojis = [];
+  //     EmojiFactory.getEmojis()
+  //       .then(function(emojis) {
+  //         emojis.data.forEach(function (code) {
+  //           $scope.Emojis.push(code);
+  //         });
+  //       });
+  //   }
+  // ])
   .controller('toggleViewController', function ($scope) {
     $scope.show = true;
     $scope.$watch('show', function () {
