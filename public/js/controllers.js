faacbaa0f7426a876fca1ead380c93abd8d17179
Spacekins <spacekins@zoho.com>
2016-10-15 15:05:40 -1000

    migrated flat map to angular direcitve

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,10 +2,13 @@ angular.module('omniMood')
   .controller('tweetController', [
     '$scope',
     'socket',
-    function ($scope, socket) {
+    function($scope, socket) {
       $scope.Tweets = [];
+      $scope.moodMin = -10;
+      $scope.moodMid = 0;
+      $scope.moodMax = 10;
       socket.emit('start tweets', true);
-      socket.on('tweet', function (tweet) {
+      socket.on('tweet', function(tweet) {
         $scope.coordinates = tweet.coordinates;
         $scope.Tweets.push(tweet.emojis);
       });
@@ -18,15 +21,15 @@ angular.module('omniMood')
       $scope.Emojis = [];
       EmojiFactory.getEmojis()
         .then(function(emojis) {
-          emojis.data.forEach(function (code) {
+          emojis.data.forEach(function(code) {
             $scope.Emojis.push(code);
           });
         });
     }
   ])
-  .controller('toggleViewController', function ($scope) {
+  .controller('toggleViewController', function($scope) {
     $scope.show = true;
-    $scope.$watch('show', function () {
+    $scope.$watch('show', function() {
       $scope.toggleText = $scope.show ? '3D View' : '2D View';
     });
   });
