e7399978ef1fd43d9fa5637ce2f706cc62c1a0f6
javenkn <javenkn7@yahoo.com>
2016-10-16 18:01:16 -1000

    adds a drop down select menu to search for a country on the globe

--- public/js/directives.js
+++ public/js/directives.js
@@ -237,6 +237,10 @@ angular.module('omniMood')
         .attr('class', 'graticule')
         .attr('d', path);
 
+      var countryList = d3.select('.sideBar')
+        .append('select')
+        .attr('class','countryList');
+
       var countryToolTip = d3.select(element[0])
         .append('div')
         .attr('class', 'countryToolTip')
@@ -244,10 +248,21 @@ angular.module('omniMood')
 
       d3.json('../json/countries.json', function(err, world) {
         countries = topojson.feature(world, world.objects.countries).features;
+        countries.sort(function (a, b) {
+          return a.properties.name > b.properties.name ? 1 : -1;
+        });
+
+        countryList.append('option')
+        .text('Choose a country')
+        .attr('disabled', true);
 
         countries.forEach(function(country) {
           countryById[country.properties.iso_n3] = country.properties.name;
+          option = countryList.append('option');
+          option.text(country.properties.name);
+          option.property('value', country.properties.iso_n3);
         });
+
         drawCountries('country', countries);
       });
 
@@ -301,14 +316,13 @@ angular.module('omniMood')
               .style('top', (d3.event.pageY + 10) + 'px');
           })
           .on('click', function(d) {
-            timer_ret_val = true;
             d3.selectAll('.selected')
               .classed('selected', false);
-
             d3.select(this)
               .select('path')
               .classed('selected', true);
             rotateToFocus(d);
+            countryList.property('value', this.id.slice(2));
           });
 
         set.append('path')
@@ -319,10 +333,31 @@ angular.module('omniMood')
           .attr('class', 'overlay')
           .attr('d', path);
 
+        countryList
+          .on('change', function () {
+            var selectedCountryId = countryList.node().value;
+            var selectedCountry = getSelectedCountry(countries, selectedCountryId);
+            d3.selectAll('.selected')
+              .classed('selected', false);
+            var selectedNode = d3.select('g#cc' + selectedCountryId).node();
+            d3.select(selectedNode)
+              .select('path')
+              .classed('selected', true);
+            rotateToFocus(selectedCountry);
+          });
+
         return set;
       }
 
+      function getSelectedCountry(countries, id) {
+        for(var i = 0; i < countries.length; i++) {
+          if(countries[i].properties.iso_n3 === id)
+            return countries[i];
+        }
+      }
+
       function rotateToFocus(d) {
+        timer_ret_val = true;
         var coords = d3.geoCentroid(d);
         coords[0] = -coords[0];
         coords[1] = -coords[1];
