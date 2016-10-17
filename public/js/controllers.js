e5385acec63c68c390268f9ea5170daff3045cd4
kekuso <kekuso808@gmail.com>
2016-10-16 16:20:57 -1000

    used String.fromCodePoint to compare array of png file references to incoming tweet emojis.

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,8 +9,19 @@ angular.module('omniMood')
       socket.emit('start tweets', true);
       socket.on('tweet', function (tweet) {
         $scope.tweet = tweet;
+        var htmlCodes = ["0x1F602", "0x2764", "0x2665", "0x1F60D", "0x1F60A", "0x1F618", "0x1F495", "0x263A", "0x1F601", "0x1F609", "0x1F44D", "0x1F60C", "0x1F60E", "0x270C", "0x1F604", "0x1F496", "0x1F61C", "0x1F603", "0x1F606", "0x1F63B", "0x1F600", "0x1F61A", "0x1F607", "0x1F639", "0x1F619", "0x1F44C", "0x1F614", "0x1F60F", "0x1F605", "0x1F611", "0x1F615", "0x1F610", "0x1F62A", "0x1F61D", "0x1F613", "0x1F623", "0x1F61B", "0x1F62C", "0x1F627", "0x1F635", "0x1F62E", "0x1F62F", "0x1F62D", "0x1F612", "0x1F629", "0x1F622", "0x1F494", "0x1F61E", "0x1F60B", "0x1F631", "0x1F621", "0x1F625", "0x1F62B", "0x1F624", "0x1F637", "0x1F620", "0x1F616", "0x1F630", "0x1F628", "0x1F61F"];
+        var emojiCodes = [];
+        for(var i = 0; i < htmlCodes.length; i++) {
+          emojiCodes[i] = String.fromCodePoint(htmlCodes[i]);
+        }
+        for(i = 0; i < tweet.emojis.length; i++) {
+          console.log(tweet.emojis[i].toString());
+          if(emojiCodes.indexOf(tweet.emojis[i].toString()) !== -1) {
+            console.log("match found!");
+          }
+        }
+        $scope.Tweets.push(tweet.emojis);
       });
-
       var emojiArray = [];
       EmojiFactory.getEmojis()
         .then(function(emojis) {
@@ -33,7 +44,6 @@ angular.module('omniMood')
                 emojiMetrics = {};
               }
             }
-            console.log(emojiMetricsArray);
 
             var emojiObject = {};
             $scope.Emojis = emojiMetricsArray.map(function(value, index) {
