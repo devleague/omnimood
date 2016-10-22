73b51dbc7d212833f37000f7c8484ff4fa8f3107
Spacekins <spacekins@zoho.com>
2016-10-22 10:40:40 -1000

    added Flat Ping to Directives

---
+++ public/js/directives/directivesPing.js
@@ -0,0 +1,244 @@
+angular.module('omniMood')
+  .directive('whateveryouwant', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        "tweet": '='
+      },
+      link: somethingelse
+    };
+
+    var zoom = d3.behavior.zoom()
+      .scaleExtent([1, 10])
+      .on("zoom", zoomed);
+
+    function somethingelse(scope, element, attr) {
+
+      var scaleWheelMin = 5;
+      var scaleWheelMax = 20;
+
+      var scaleZoomTo = 5;
+      var isZoomed = false;
+      var currentZoom = 1;
+      var currentZoomMouseX = 0;
+      var currentZoomMouseY = 0;
+
+      d3.select("div.mapContainer").attr("height",1000);
+
+      var mapSVG = d3.select(element[0]).append("svg")
+        .attr("id", "svg_map"),
+        width = (window.innerWidth  * .85), //.85 document.body.clientWidth
+        height = (window.innerHeight * .85),
+        outlineDefault = "#eeeeee",
+        outlineHighlight = "#1221ee",
+        fillDefault = "#000000",
+        moodMin = -10,
+        moodMid = 0,
+        moodMax = 10,
+        countryArrayIndex = 0;
+
+      var moodScale = d3.scaleLinear()
+        .domain([moodMin, moodMid, moodMax])
+        .range(["red", "yellow", "green"]);
+
+      var latLng = [
+        [-57.50617017, -25.24155981]
+      ];
+
+      latLng = [
+        [107.75749781, -6.9674592]
+      ];
+
+      var mapRect = mapSVG
+        .attr("width", width)
+        .attr("height", height)
+        //.call("zoom",zoomTo)
+
+      .append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .style("fill", "steelblue");
+
+      var mapGroup = mapSVG.append("g").on('click', function() {
+          zoomTo(this)
+        })
+        .on('dblclick', zoomOut)
+        .on('wheel', zoomIn);
+
+      //  var codeToCountry; // You are on your own!  10/15/16
+
+      var emojiList;
+      d3.json("./json/codeEmoji.json", function(error, thisEmojiList) {
+        emojiList = thisEmojiList;
+      });
+
+      function getCenter(newPosition, objectCenter, toScale) {
+        console.log(newPosition, objectCenter, toScale);
+        return (isZoomed ? 0 : (newPosition - (objectCenter * toScale)));
+      }
+
+      function zoomTo(scaleTo) {
+        console.log("test");
+        // coordinates = d3.mouse(mapGroup);
+        let mouse = d3.mouse(d3.event.currentTarget);
+        currentZoomMouseX = mouse[0];
+        currentZoomMouseY = mouse[1];
+
+        mapGroup.transition()
+          .duration(2000)
+          .attr("transform", "translate(" + getCenter(width / 2, mouse[0], scaleZoomTo) + "," + getCenter(height / 2, mouse[1], scaleZoomTo) + ") scale (" + (isZoomed ? 1 : scaleZoomTo) + ")") //scaleZoomTo
+
+        d3.selectAll("path")
+          .attr("stroke-width", function() {
+            return isZoomed ? 1 : 0.1
+          });
+
+
+        isZoomed = false;
+        currentZoom = scaleZoomTo;
+      }
+
+      function zoomOut(scaleTo) {
+        mapGroup.transition()
+          .duration(2000)
+          .attr("transform", "translate(" + 0 + "," + 0 + ") scale (" + 1 + ")")
+
+        d3.selectAll("path")
+          .attr("stroke-width", function() {
+            return 1;
+          });
+
+        isZoomed = false;
+      }
+
+      function zoomIn() {
+        var toZoomIn = false;
+
+        if (currentZoom >= scaleWheelMin && currentZoom <= scaleWheelMax) {
+          if (d3.event.wheelDelta == -120) {
+            currentZoom = (currentZoom > scaleWheelMin) ? --currentZoom : currentZoom;
+            toZoomIn = true;
+          } else {
+            currentZoom = (currentZoom < scaleWheelMax) ? ++currentZoom : currentZoom;
+            toZoomIn = true;
+          }
+
+
+          if (toZoomIn) {
+            console.log("translate(" + currentZoomMouseX + "," + currentZoomMouseY + ")");
+            mapGroup.transition()
+              .duration(1000)
+              .attr("transform", "translate(" + getCenter(width / 2, currentZoomMouseX, currentZoom) + "," + getCenter(height / 2, currentZoomMouseY, currentZoom) + ") scale (" + currentZoom + ")")
+
+            d3.selectAll("path")
+              .attr("stroke-width", function() {
+                return 1;
+              });
+          }
+        }
+
+        d3.selectAll("path")
+          .attr("stroke-width", function() {
+            return 0.1;
+          });
+
+        isZoomed = false;
+      }
+
+      //countries_no_show_antarctica.json world-50m_DoNotShowAntarctica.json
+
+      d3.json("../json/world-50m_DoNotShowAntarctica.json", function(error, world) {
+        var countries = topojson.feature(world, world.objects.countries).features;
+        var projection = d3.geoMercator()
+          .scale(233) //280
+          .translate([width / 2, height / 3.6]) //width / 2.28
+          .center([0, 50]); //[-106, 37.5]
+
+        var path = d3.geoPath()
+          .projection(projection);
+
+        mapGroup.selectAll(".country")
+          .data(countries)
+          .enter().insert("path", ".graticule")
+          .attr("id", function(d) {
+            return "cc" + (d.properties.iso_n3 / 1);
+          })
+          .attr("d", path)
+          .attr("stroke", outlineDefault)
+          .on("mouseover", function(d) {
+            d3.select(this)
+              .attr("stroke", outlineHighlight)
+              .style("fill", "green");
+          })
+          .on("mouseout", function() {
+            d3.select(this)
+              .attr("stroke", outlineDefault)
+              .style("fill", fillDefault);
+          })
+          .append("svg:title")
+          .text(function(d) {
+            return d.properties.name;
+          });
+
+
+        scope.$watch('tweet', function(tweet) {
+          if (tweet) {
+            var latLngData = [
+              [tweet.coordinates.long, tweet.coordinates.lat]
+              //  [coordinates.lat, coordinates.long]
+            ];
+
+            mapGroup.append('circle')
+              .data(latLngData)
+              .attr("cx", function(d) {
+                return projection(d)[0];
+              })
+              .attr("cy", function(d) {
+                return projection(d)[1];
+              })
+              .attr("r", "300px")
+              .attr("fill", moodScale(tweet.moodValue * 10)) //"white")
+              .style("fill-opacity", 0.5)
+              .transition()
+              .duration(2000)
+              .attr("r", "3px")
+              .attr("fill", moodScale(tweet.moodValue * 10))
+              .style("fill-opacity", 0.3);
+
+            var emojiX = projection(latLngData[0])[0]; //-120;
+            var emojiY = projection(latLngData[0])[1]; //-140;
+
+
+            var surrogate = tweet.emojis.map((emoji) => {
+              return '\\u' + emoji.charCodeAt(0).toString(16).toUpperCase() + '\\u' + emoji.charCodeAt(1).toString(16).toUpperCase();
+            });
+            surrogate.forEach(function(surrogate) {
+              if (emojiList[surrogate]) {
+                mapGroup.append('image')
+                  .data(latLngData)
+                  .attr('xlink:href', '../emojis/' + emojiList[surrogate].code.toLowerCase() + '.png')
+                  .attr('x', 0)
+                  .attr('y', 0)
+                  .attr("x", function(d) {
+                    return emojiX;
+                  })
+                  .attr("y", function(d) {
+                    return emojiY;
+                  })
+                  .attr('height', 300)
+                  .attr('width', 200)
+                  .style('fill-opacity', 0.5)
+                  .transition()
+                  .duration(1000)
+                  .attr('height', 3)
+                  .attr('width', 2);
+              } else {
+                //  console.log(surrogate);
+              }
+            });
+          }
+        })
+      });
+    }
+  })
+
