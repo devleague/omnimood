9402c47e2d8a6181718767e7f0cefc19745f2bbb
javenkn <javenkn7@yahoo.com>
2016-10-24 01:26:31 -1000

    emoji cloud is working and looks coolgit add public/js/directives/emojiCloud.js! XD

--- public/js/directives/emojiCloud.js
+++ public/js/directives/emojiCloud.js
@@ -9,10 +9,11 @@ angular.module('omniMood')
       };
 
       function link (scope, element, attr) {
-        var width = 640;
-        var height = 480;
-        var emojiList = [];
-        var emojiCodeList;
+        var width = 450;
+        var height = 450;
+        var emojiList = [],
+            emojiCodeList,
+            highestCount = 0;
 
         var svg = d3.select(element[0])
           .append('svg')
@@ -23,10 +24,7 @@ angular.module('omniMood')
           .attr('class', 'node')
           .attr('transform', "translate(" + width/2 + "," + height/2 + ")");
 
-        var emojiSize = d3.scaleLinear()
-          .domain([5, 100])
-          .range([10, 80]);
-
+        var defs = d3.select('.emojiCloud').append('svg:defs');
 
         d3.json('/json/codeEmoji.json', function (emojiObject) {
           emojiCodeList = emojiObject;
@@ -38,14 +36,37 @@ angular.module('omniMood')
               return '\\u' + emoji.charCodeAt(0).toString(16).toUpperCase() + '\\u' + emoji.charCodeAt(1).toString(16).toUpperCase();
             });
 
-            surrogate.forEach(function(surrogate) {
-              if (emojiCodeList[surrogate]) {
-                checkForEmoji(surrogate, emojiCodeList[surrogate].code);
-              }
-            });
+            var emojiSize = d3.scaleLinear()
+              .domain([1, 50])
+              .range([7, 50]);
 
             var simulation = d3.forceSimulation()
-              .force("collide", d3.forceCollide( function(d){ return d.r + 1; }).iterations(4));
+              .force('center', d3.forceCenter())
+              .force('charge', d3.forceManyBody().strength(0.5).distanceMin(5))
+              .force("collide", d3.forceCollide(function (d) {
+                return emojiSize(d.counter);
+              }).iterations(4));
+
+            emojiList.sort(function (a, b) {
+              return b.counter - a.counter; // decreasing order
+            });
+
+            surrogate.forEach(function(surrogate) {
+              if (emojiCodeList[surrogate]) {
+                checkForEmoji(surrogate, emojiCodeList[surrogate].code, simulation);
+              }
+            });
+            highest = emojiList[0].counter;
+            console.log(highest);
+            if(highest > 50) {
+              emojiSize = d3.scaleLinear()
+              .domain([1, highest])
+              .range([7, 50]);
+            }
+
+            simulation
+              .nodes(emojiList)
+              .on('tick', tick);
 
             var node = svg.selectAll('.emojis')
               .data(emojiList);
@@ -54,45 +75,75 @@ angular.module('omniMood')
 
             node
               .enter()
-                .append('image')
+                .append('g')
+                .append('circle')
                 .attr('class', 'emojis')
-                .attr('xlink:href', function (d, i) {
-                  return '../../emojis/' + d.code.toLowerCase() + '.png';
+                .style('fill', function (d) {
+                  return 'url('+ '#' + d.code.toUpperCase() + ')';
                 })
-                .attr('x', function (d) {
+                // .attr('xlink:href', function (d, i) {
+                //   return '../../emojis/' + d.code.toLowerCase() + '.png';
+                // })
+                .merge(node)
+                .attr('cx', function (d) {
                   return d.x;
                 })
-                .attr('y', function (d) {
+                .attr('cy', function (d) {
                   return d.y;
                 })
-                .merge(node)
-                .attr('width', function (d, i) {
-                  return d.counter;
-                })
-                .attr('height', function (d, i) {
-                  return d.counter;
-                })
+                .attr('r', function (d) { return emojiSize(d.counter); })
+                // .attr('height', function (d) { return d.counter; })
                 .attr('id', function (d, i) {
                   return d.code;
                 });
 
-            var tick = function () {
-              node
+            var defsPattern = defs.selectAll('.imgPattern')
+              .data(emojiList);
+
+            defsPattern.exit().remove();
+
+            defsPattern
+              .enter().append('pattern')
+                .attr('class', 'imgPattern')
+                .attr('height', '1')
+                .attr('width', '1')
+                .attr("patternUnits", "objectBoundingBox")
+                .attr('id', function (d) {
+                  return d.code;
+                })
+                .append('image')
+                .attr('class', 'pic')
+                .attr('xlink:href', function (d, i) {
+                  return '../../emojis/' + d.code + '.png';
+                })
+                .merge(defsPattern)
+                .select('.pic')
+                .attr('height', function (d) {
+                  return emojiSize(d.counter);
+                })
+                .attr('width', function (d) {
+                  return emojiSize(d.counter);
+                })
                 .attr('x', function (d) {
-                  return d.x;
+                  return ((Math.sqrt(2*(emojiSize(d.counter) * emojiSize(d.counter))))-emojiSize(d.counter))/2;
                 })
                 .attr('y', function (d) {
+                  return ((Math.sqrt(2*(emojiSize(d.counter) * emojiSize(d.counter))))-emojiSize(d.counter))/2;
+                });
+
+            var tick = function() {
+              node
+                .attr('cx', function (d) {
+                  return d.x;
+                })
+                .attr('cy', function (d) {
                   return d.y;
                 });
             };
-
-            simulation
-              .nodes(emojiList)
-              .on('tick', tick);
           }
         });
 
-        function checkForEmoji (surrogate, emojiCode) {
+        function checkForEmoji (surrogate, emojiCode, simulation) {
           var isEmojiFound = emojiList.filter(function (emojiObj) {
             return emojiObj.code === emojiCode;
           });
