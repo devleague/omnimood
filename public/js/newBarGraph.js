44b88e937d1845a03932fa95316e2947fbbdb16b
javenkn <javenkn7@yahoo.com>
2016-10-25 01:40:08 -1000

    fixes svg width and height for bar graph

--- public/js/newBarGraph.js
+++ public/js/newBarGraph.js
@@ -54,8 +54,8 @@ getJSON('/api/timeline').then(function(data) {
     var topEmojis = emojiArray.slice(emojiArray.length - 10, emojiArray.length);
     // set the dimensions and margins of the graph
     var margin = {top: 40, right: 20, bottom: 50, left: 40},
-        width = 350 - margin.left - margin.right,
-        height = 250 - margin.top - margin.bottom;
+        width = parseInt(d3.select('.barGraph').style('width')) - margin.left - margin.right,
+        height = parseInt(d3.select('.barGraph').style('height')) - margin.top - margin.bottom;
 
     // set the ranges
     var x = d3.scaleBand()
